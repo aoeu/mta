73a75676a0dda94cffc6b79a73760081e13e3f40
Travis <travis@mcdem.us>
2015-02-17 21:46:15 -0500

    Serve all lines on the root URL.

--- examples/microservice.go
+++ examples/microservice.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"strings"
@@ -19,17 +20,45 @@ func main() {
 	if err != nil {
 		log.Fatalf("Problem getting subway status: %v", err)
 	}
+	allLineNames := make([]string, 0)
 	for _, lineName := range []string{"1", "2", "3", "4", "5", "6", "7", "A", "C", "E", "B", "D", "F", "M", "G", "J", "Z", "L", "N", "Q", "R", "S", "SIR"} {
-		handler := serve(service, lineName)
+		handler := serveLine(service, lineName)
 		http.HandleFunc("/"+lineName, handler)
 		if strings.ToUpper(lineName) != strings.ToLower(lineName) {
-			http.HandleFunc("/"+strings.ToLower(lineName), handler)
+			lineName := strings.ToLower(lineName)
+			allLineNames = append(allLineNames, lineName)
+			http.HandleFunc("/"+lineName, handler)
 		}
 	}
+	http.HandleFunc("/", serveAllLines(allLineNames))
 	http.ListenAndServe(*port, nil)
 }
 
-func serve(service mta.Service, lineName string) func(w http.ResponseWriter, r *http.Request) {
+var allLinesTempl = `
+<html>
+	<head>
+		<title>MTA Service Status</title>
+	</head>
+	<body>
+	{{range $i, $lineName := .}}
+		<a href="/{{.}}">{{.}}</a><br/>
+	{{end}}
+	</body>
+</html>
+`
+
+func serveAllLines(lineNames []string) func(w http.ResponseWriter, r *http.Request) {
+	templName := "all_train_lines"
+	templ, err := template.New(templName).Parse(allLinesTempl)
+	if err != nil {
+		log.Fatalf("Failed to parse template %v with error %v", templName, err)
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		templ.Execute(w, lineNames)
+	}
+}
+
+func serveLine(service mta.Service, lineName string) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		line, err := mta.GetLine(service, lineName)
 		if err != nil {
