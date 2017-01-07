c2f071e449124766bb72647d0ba14daca00b846e
Travis McDemus <travis@mcdem.us>
2017-01-07 16:57:55 -0500

    Add command to print next train arrival at a specific stop

---
+++ cmd/printNextLTrain.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"flag"
+	"github.com/aoeu/mta"
+	"log"
+)
+
+func main() {
+	var key string
+	flag.StringVar(&key, "key", "aoeu", "Open sesame.")
+	flag.Parse()
+	getAndLogTrainTime(key)
+}
+
+func getAndLogTrainTime(key string) {
+	t, err := mta.GetNextMontroseLTrains(key)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, s := range t[0:1] {
+		log.Println(s)
+	}
+
+}
