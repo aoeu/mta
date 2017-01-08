5e5d4664faed0b47d61bec0e97df4ccc66b0ae3d
Travis McDemus <travis@mcdem.us>
2017-01-08 18:03:25 -0500

    Wait until all data is downloaded before writing to file

--- cmd/dumpLTrainsForLCD.sh
+++ cmd/dumpLTrainsForLCD.sh
@@ -3,8 +3,9 @@
 outputFilepath="/tmp/l_train_times_and_status_for_lcd.txt"
 
 main() {
-	printNextLTrainsForLCD -key "$1" > $outputFilepath && \
-	printStatusOfLTrain | grep --after-context 1 'Planned Work' >> $outputFilepath
+	printNextLTrainsForLCD -key "$1" > $outputFilepath.$$ && \
+	printStatusOfLTrain | grep --after-context 1 'Planned Work' | tail -1 >> $outputFilepath.$$ && \
+	mv $outputFilepath.$$ $outputFilepath
 }
 
 main $*
